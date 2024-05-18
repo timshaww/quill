0fcaa552a75bc688a03a380a898096f9a6123d61
timshaww <time4shaw@gmail.com>
2024-05-18 16:26:47 -0400

    init

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,17 @@
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
+import { redirect } from 'next/navigation';
+
+const page = async () => {
+	const { getUser } = getKindeServerSession();
+	const user = await getUser();
+
+	if (!user || !user.id) redirect('/auth-callback?origin=dashboard');
+
+	return (
+		<div>
+			<div>{user.email}</div>
+		</div>
+	);
+};
+
+export default page;
